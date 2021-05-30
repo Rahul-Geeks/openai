348c443ef38223b7de5bf02d1523b14e251fab3b
Rahul Chouhan <rahul@xamplay.com>
2021-05-30 14:45:24 +0530

    Adding options to prompt text completion

--- server.js
+++ server.js
@@ -7,22 +7,34 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;      // Openai API key
 
 const openai = new OpenAI(OPENAI_API_KEY);
 
-// Complete text
-app.get("/complete/text", (req, res) => {
-    if (req.query.prompt) {                             // Don't allow if prompt is not passed
-        let params = {
-            engine: 'davinci',
-            prompt: req.query.prompt,
-            maxTokens: 800,
-            temperature: 0.7,
-            topP: 1,
-            presencePenalty: 0,
-            frequencyPenalty: 1
-        };
+/**
+ * Get completed string
+ * @param {string} prompt Prompt string
+ * @returns Promise with completed
+ */
+async function getCompleteText (prompt) {
+    let complete = await openai.complete({
+        engine: 'davinci',
+        prompt: prompt,
+        maxTokens: 800,
+        temperature: 0.7,
+        topP: 1,
+        presencePenalty: 0,
+        frequencyPenalty: 1,
+        bestOf: 1,
+        n: 1,
+        stream: false,
+        stop: ['\n', "testing"]
+    });
 
-        // Call completion API
-        openai.complete(params)
-            .then(textObj => res.status(200).json(textObj.data))
+    return complete.data;
+}
+
+// Complete text
+app.get("/complete/text", async (req, res) => {
+    if (req.query.prompt) {                             // Don't allow if prompt is not passed
+        getCompleteText(req.query.prompt)               // Call completion API
+            .then(textObj => res.status(200).json(textObj))
             .catch(error => res.status(500).json({ message: 'Error occurred while getting completed text', error: error.toString() }));
     }
     else
